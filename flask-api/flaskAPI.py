18fcc1bf67ba35a5b52ac4f193f59d3eb94d1c67
Yudi Tan <yudi98@berkeley.edu>
2017-12-20 22:18:10 -0600

    update

--- flask-api/flaskAPI.py
+++ flask-api/flaskAPI.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'an-api-per-day'
-app.config['MONGO_URI'] = 'mongodb://<dbuser>:<dbpassword>@ds161016.mlab.com:61016/an-api-per-day'
+app.config['MONGO_URI'] = 'mongodb://<dbusername>:<dbpassword>@ds161016.mlab.com:61016/an-api-per-day'
 mongo = PyMongo(app)
+CORS(app)
 
 @app.route('/posts', methods=['GET'])
 def PostList():
--- sample-frontend/src/App.js
+++ sample-frontend/src/App.js
@@ -13,8 +13,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:8000/posts/').then((response) => {
-      var data = response.data.map(d => <p key={d.title}> Title - {d.title} <br/> Content - {d.content} </p>);
+    axios.get('http://localhost:5000/posts').then((response) => {
+      var data = response.data.result.map(d => <p key={d.title}> Title - {d.title} <br/> Content - {d.content} </p>);
       this.setState({data});
     })
   }
